110ec96e500cc30f92fc37f299366f5451039f94
Peter Zhang <p@peetahzee.com>
2012-10-29 12:32:27 -0700

    Skeleton for Java project

---
+++ Factory Project/src/Server.java
@@ -0,0 +1,14 @@
+
+/**
+ * The Server is the "middleman" between Agents and the GUI clients. 
+ * This is where constructors of the different Agents will be called, 
+ * as well as establishing connections with the GUI clients.
+ * 
+ * @author Peter Zhang
+ *
+ */
+public class Server {
+	public static void main (String[] args) {
+		System.out.println("Dummy text.");
+	}
+}
