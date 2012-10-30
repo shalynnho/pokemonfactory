022bfcceea6864c544caae49cdec99d1ec5f097e
Ross Newman <rcnewman@usc.edu>
2012-10-30 15:22:14 -0700

    Interfaces for Camera and PartsRobot

--- Factory Project/src/factory/interfaces/Camera.java
+++ Factory Project/src/factory/interfaces/Camera.java
@@ -1,6 +1,16 @@
 package factory.interfaces;
 
+import java.util.List;
+
+import factory.Kit;
+import factory.NestAgent;
+
 public interface Camera {
 
+	public abstract void msgInspectKit(Kit kit);
+	public abstract void msgIAmFull(NestAgent nest);
+	public abstract void msgTakePictureNestDone(List<GUIPart> parts, NestAgent nest);
+	public abstract void msgTakePictureKitDone(boolean done);
+	
 	public abstract boolean pickAndExecuteAnAction();
 }
