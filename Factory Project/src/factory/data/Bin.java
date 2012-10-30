cac877bb21916476973f5095df6baa0cfae93a12
Michael Gendotti <mgendott@usc.edu>
2012-10-30 14:57:58 -0700

    Added Part and Bin

---
+++ Factory Project/src/factory/data/Bin.java
@@ -0,0 +1,9 @@
+package factory.data;
+
+import java.util.ArrayList;
+
+public class Bin {
+	public GUIBin bin;
+	public PartType type;
+	public ArrayList<Part> parts;
+}
---
+++ Factory Project/src/factory/data/Part.java
@@ -0,0 +1,10 @@
+package factory.data;
+
+public class Part {
+
+	public GUIPart part;
+	public PartType type;
+
+	public enum PartType {A,B,C,D,E,F,G,H};
+	
+}
