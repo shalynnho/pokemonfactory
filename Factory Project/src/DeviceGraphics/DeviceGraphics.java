cdef73db8e870f30a0cf7fd7b5f4dce25e203477
Peter Zhang <p@peetahzee.com>
2012-10-31 11:02:34 -0700

    Finishing DeviceGraphics abstract class, and adding in Location in Utils

--- Factory Project/src/DeviceGraphics/DeviceGraphics.java
+++ Factory Project/src/DeviceGraphics/DeviceGraphics.java
@@ -1,5 +1,22 @@
 package DeviceGraphics;
 
-public abstract class DeviceGraphics {
+import Networking.Server;
+import Utils.Location;
 
+/**
+ * The abstract class that supports all other DeviceGraphics.
+ * 
+ * @author Peter Zhang
+ */
+public abstract class DeviceGraphics {
+	private Server server;
+	private Location location;
+	
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
 }
