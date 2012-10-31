821a9a2722855bbc86e965c89f232463b8844cee
Vansh Jain <vanshjain@gmail.com>
2012-10-31 12:31:02 -0700

    worked on PartsRobotGraphics

--- eclipse_files/src/DeviceGraphics/PartsRobotGraphics.java
+++ eclipse_files/src/DeviceGraphics/PartsRobotGraphics.java
@@ -13,7 +13,66 @@ public class PartsRobotGraphics {
 	boolean arm1, arm2, arm3, arm4; // whether the arm is full, initialized empty
 	ArrayList<Part> partArray; // an array of parts that allocates memory for 4 parts
 	
-	public void PartsRobotGrpahics
+	public void pickUpPart(Part){
+		/**pickup from nests
+		 * goes to a location to pick up a part
+		 * be able to hold 4 parts at a time
+		 */
+	}
+	
+	public void givePartToKit(Kit, int){
+		/**gives the part to the kit
+		 * goes to a location to give a part
+		 * puts part in a specific location inside the kit
+		 */
+	}
+	
+	public void rotateArm(){
+		/**
+		 * rotates the arm
+		 */
+	}
+	
+	
+	 
+	public  boolean isFullArm1(arm1){
+		/**
+		 *  returns true if arm1 is full
+		 */
+	}
+	
+	public boolean isFullArm2(arm2){
+		/**
+		 *  returns true if arm2 is full
+		 */
+	}
+	
+	public boolean isFullArm3(arm3){
+		/**
+		 *  returns true if arm3 is full
+		 */
+	}
+	
+	public boolean isFullArm4(arm4){
+		/**
+		 * returns true if arm4 is full
+		 */
+	}
+	
+	public void goHome(){
+		/**
+		 * depends on specific graphics implementation
+		 * sends the robot back to the initial location
+		 * makes sure that the robot doesn't collide
+		 * then send message that the action has been performed(either part is picked up or it's given to a kit)
+		 */
+	}
+	
+	public Location getLocation(){
+		/**
+		 * returns the current Location
+		 */
+	}
 	
 	
 }
