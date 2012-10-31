119b52df583f4de4e71f264290c79956e0cd5771
Harry Trieu <htrieu@usc.edu>
2012-10-31 11:05:32 -0700

    Made changes to feeder.

--- Factory Project/src/DeviceGraphics/FeederGraphics.java
+++ Factory Project/src/DeviceGraphics/FeederGraphics.java
@@ -1,6 +1,41 @@
 package DeviceGraphics;
 
-public class FeederGraphics extends DeviceGraphics  {
+import til.Location;
+import Networking.Server;
 
+public class FeederGraphics extends DeviceGraphics  {
+	Server server;
+	Location location;
+	int feederID;
+	int partsFed;
+	// BinGraphics binGraphics;
+	// boolean partLow;
+	
+	/**
+	 * 
+	 * @param id
+	 * @param myServer
+	 */
+	FeederGraphics(int id, Server myServer) {
+		id = feederID;
+		server = myServer;
+		partsFed = 0;
+		
+		// TODO edit location coordinates later
+		location = new Location(200, 100*feederID);
+		
+		// TODO send message to FeederGraphicsDisplay
+		
+	}
+	
+	void movePartToLane(Part) {
+		
+	}
+	
+	void flipDiverter() {
+		
+	}
+	
+	
 
 }
---
+++ Factory Project/src/DeviceGraphicsDisplay/FeederGraphicsDisplay.java
@@ -0,0 +1,13 @@
+package DeviceGraphicsDisplay;
+
+public class FeederGraphicsDisplay extends DeviceGraphicsDisplay {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
