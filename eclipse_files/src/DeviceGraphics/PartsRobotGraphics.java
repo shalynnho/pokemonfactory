5f9eaf46c06ac5d9dd4f9d171cf2ef5df32243a2
Vansh Jain <vanshjain@gmail.com>
2012-10-31 12:24:00 -0700

    Began PartsRobotGraphics

--- eclipse_files/src/DeviceGraphics/PartsRobotGraphics.java
+++ eclipse_files/src/DeviceGraphics/PartsRobotGraphics.java
@@ -8,4 +8,12 @@ package DeviceGraphics;
 
 public class PartsRobotGraphics {
 
+	Location initialLocation; // initial location of robot
+	Location currentLocation; // current location of robot
+	boolean arm1, arm2, arm3, arm4; // whether the arm is full, initialized empty
+	ArrayList<Part> partArray; // an array of parts that allocates memory for 4 parts
+	
+	public void PartsRobotGrpahics
+	
+	
 }
