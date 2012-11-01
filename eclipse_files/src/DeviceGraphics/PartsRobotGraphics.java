363ddf19ae2dc60171af76371f155531d90736a5
James Li <jameswli@usc.edu>
2012-10-31 22:11:26 -0700

    partsrobot

--- eclipse_files/src/DeviceGraphics/PartsRobotGraphics.java
+++ eclipse_files/src/DeviceGraphics/PartsRobotGraphics.java
@@ -1,5 +1,7 @@
 package DeviceGraphics;
 
+import factory.data.Part;
+
 /**
  * The parts robot will be used to move 
  * @author vanshjain
@@ -10,10 +12,33 @@ public class PartsRobotGraphics {
 
 	Location initialLocation; // initial location of robot
 	Location currentLocation; // current location of robot
-	boolean arm1, arm2, arm3, arm4; // whether the arm is full, initialized empty
+	boolean arm1, arm2, arm3, arm4; // whether the arm is full,Parttialized empty
 	ArrayList<Part> partArray; // an array of parts that allocates memory for 4 parts
+	KitGraphics kit;
+	
+	public PartsRobotGraphics {
+		initialLocation = new Location();
+		currentLocation = initialLocation;
+		arm1 = false;
+		arm2 = false;
+		arm3 = false;
+		arm4 = false;
+		partArray = new ArrayList<Part>();
+	}
+	
 	
 	public void pickUpPart(Part part, Location location){
+		currentLocation = location;
+		Animation(currentLocation, 10);
+		partArray.add(part);
+		if (!isFullArm1)
+			arm1 = true;
+		else if (!isFullArm2)
+			arm2 = true;
+		else if (!isFullArm3)
+			arm3 = true;
+		else if (!isFullArm4)
+			arm4 = true;	
 		/**
 		 * pickup from nests
 		 * goes to a location to pick up a part
@@ -22,6 +47,20 @@ public class PartsRobotGraphics {
 	}
 	
 	public void givePartToKit(Kit, int){
+		Animation(Kit.location, 10);
+		while (isFullArm1){
+			partArray.remove(add);
+			if (isFullArm4)
+				arm4 = false;
+			else if (isFullArm3)
+				arm3 = false;
+			else if (isFullArm2)
+				arm2 = false;
+			else if (isFullArm1)
+				arm1 = false;
+		}
+		
+		goHome();
 		/**
 		 * gives the part to the kit
 		 * goes to a location to give a part
@@ -37,31 +76,38 @@ public class PartsRobotGraphics {
 	
 	
 	 
-	public  boolean isFullArm1(arm1){
+	public boolean isFullArm1(){
+		return arm1;
 		/**
 		 *  returns true if arm1 is full
 		 */
 	}
 	
-	public boolean isFullArm2(arm2){
+	
+	public boolean isFullArm2(){
+		return arm2;
 		/**
 		 *  returns true if arm2 is full
 		 */
 	}
 	
-	public boolean isFullArm3(arm3){
+	public boolean isFullArm3(){
+		return arm3;
 		/**
 		 *  returns true if arm3 is full
 		 */
 	}
 	
-	public boolean isFullArm4(arm4){
+	public boolean isFullArm4(){
+		return arm4;
 		/**
 		 * returns true if arm4 is full
 		 */
 	}
 	
 	public void goHome(){
+		Animation(initialLocation, 10);
+		currentLocation = initialLocation;
 		/**
 		 * depends on specific graphics implementation
 		 * sends the robot back to the initial location
@@ -71,6 +117,7 @@ public class PartsRobotGraphics {
 	}
 	
 	public Location getLocation(){
+		return currentLocation;
 		/**
 		 * returns the current Location
 		 */
---
+++ eclipse_files/src/DeviceGraphicsDisplay/PartsRobotDisplay.java
@@ -0,0 +1,37 @@
+package DeviceGraphicsDisplay;
+
+import Networking.*;
+
+public class PartsRobotDisplay extends DeviceGraphicsDisplay {
+	
+	private static ImageIcon partsRobotImage;
+	private static ImageIcon armImage;
+	
+	public PartsRobotDisplay(){
+		
+	}
+	
+	public void paintComponent(Graphics2D g2){
+		
+	}
+
+	public void requestData(Request r){
+		
+	}
+	
+	public void pickUpPart(PartGraphicsDisplay part){
+		
+	}
+	
+	public void givePartToKit(PartGraphicsDisplay part){
+		
+	}
+	
+	public void rotateArm(){
+		
+	}
+	
+	public void movePartsRobot(){
+		
+	}
+}
