9dcf39276b010403af9cad9ce7270e49a874ee78
Chris Gebert <cgebert@usc.edu>
2012-11-02 18:31:44 -0700

    Added Draw function to DeviceGraphicsDisplay

--- eclipse_files/src/DeviceGraphicsDisplay/DeviceGraphicsDisplay.java
+++ eclipse_files/src/DeviceGraphicsDisplay/DeviceGraphicsDisplay.java
@@ -1,5 +1,12 @@
 package DeviceGraphicsDisplay;
 
+import java.awt.Graphics2D;
+
+import javax.swing.JFrame;
+
 public abstract class DeviceGraphicsDisplay {
 
+	public void Draw (JFrame myJFrame, Graphics2D g)
+		
+	
 }
