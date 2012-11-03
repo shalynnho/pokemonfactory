68e98a598ab076da55daf298d95744a12b2a7931
Peter Zhang <p@peetahzee.com>
2012-11-02 22:31:30 -0700

    Add in docs for DeviceGraphicsDisplay and add in a new method to receive data from Server

--- eclipse_files/src/DeviceGraphicsDisplay/DeviceGraphicsDisplay.java
+++ eclipse_files/src/DeviceGraphicsDisplay/DeviceGraphicsDisplay.java
@@ -4,11 +4,29 @@ import java.awt.Graphics2D;
 
 import javax.swing.JFrame;
 
+import Networking.Client;
+import Networking.Request;
 import Utils.Location;
 
+/**
+ * An abstract class that every DeviceGraphicsDisplay extends from.
+ * 
+ * @author Peter Zhang
+ */
 public abstract class DeviceGraphicsDisplay {
+	
+	Client client;
+	Location location;
 
+	/**
+	 * Override this method to draw out your component.
+	 */
 	public abstract void draw (JFrame myJFrame, Graphics2D g);
-		
+	
+	/**
+	 * Override this method so to process any requests sent from Server. Will be called by Client as Requests arrive.
+	 */
+	public abstract void receiveData(Request req);
+	
 	public abstract void setLocation (Location newLocation);
 }
