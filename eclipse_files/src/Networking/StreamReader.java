c16b65a4b39a24174de03f6a858c3b2e03a4c1bf
Peter Zhang <p@peetahzee.com>
2012-11-02 21:49:45 -0700

    (left a file out)

--- eclipse_files/src/Networking/StreamReader.java
+++ eclipse_files/src/Networking/StreamReader.java
@@ -1,5 +1,7 @@
 package Networking;
 
+import java.io.EOFException;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
 
@@ -9,20 +11,15 @@ import java.net.Socket;
  * @author Peter Zhang
  */
 public abstract class StreamReader implements Runnable{
-	private Socket socket;
 	private ObjectInputStream ois;	
 	
-	public StreamReader(Socket s){
-		socket = s;
-		try {
-			// Create the input stream for reading from the server
-			ois = new ObjectInputStream(socket.getInputStream());
-			System.out.println("StreamReader: got stream");
-		} catch (Exception e) {
-			System.out.println("StreamReader: Stream init fail");
-			e.printStackTrace();
-			System.exit(1);
-		}
+	public StreamReader(Socket s) throws IOException {
+		this(new ObjectInputStream(s.getInputStream()));
+	}
+	
+	public StreamReader(ObjectInputStream o){
+		ois = o;
+		System.out.println("StreamReader: got stream");
 	}
 	
 	@Override
@@ -32,8 +29,12 @@ public abstract class StreamReader implements Runnable{
 				Request req = (Request) ois.readObject();
 				System.out.println("StreamReader: received request = " + req.getCommand());
 				receiveData(req);
-			} catch (Exception e) {
-				System.out.println("StreamReader: Cannot read data from Server");		
+			} catch(EOFException e) {
+				System.out.println("StreamReader: Connection lost. Other terminal has discnonected.");
+				break;
+			} catch(Exception e) {
+				System.out.println("StreamReader: Cannot read data");
+				e.printStackTrace();
 				break;
 			}
 		}
