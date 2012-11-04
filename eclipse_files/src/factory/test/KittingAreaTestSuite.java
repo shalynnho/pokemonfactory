b578465cef76edf913deb9646a93eda8eee64e93
dpaje <paje@usc.edu>
2012-11-03 23:59:23 -0700

    Added a test case that tests the KitRobotAgent. The test involves the fulfillment of an order of a specified number of kits (currently 100).
    Also added a test suite that will be used to run the test cases for the
    remaining agents in the kitting area (WIP).

---
+++ eclipse_files/src/factory/test/KittingAreaTestSuite.java
@@ -0,0 +1,24 @@
+package factory.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * This test suite will run all JUnit tests for agents in the Kitting Area
+ * including the KitRobot, Stand and Conveyor agents.
+ * @author dpaje
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ KitRobotAgentTestNormativeScenario.class, })
+public class KittingAreaTestSuite {
+
+	/**
+	 * Empty method; runs tests
+	 */
+	@Test
+	public void runTestSuite() {
+	}
+
+}
