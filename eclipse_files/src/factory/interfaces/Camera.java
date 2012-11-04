4e29add448651436e29297c6f7d80b31d37b6815
Arjun Bhargava <arjunbha@usc.edu>
2012-11-04 14:43:31 -0800

    Commit error

--- eclipse_files/src/factory/interfaces/Camera.java
+++ eclipse_files/src/factory/interfaces/Camera.java
@@ -11,7 +11,7 @@ public interface Camera {
 
 	public abstract void msgInspectKit(Kit kit);
 	public abstract void msgIAmFull(NestAgent nest);
-	public abstract void msgTakePictureNestDone(List<PartGraphics> parts, Nest nest);
+	public abstract void msgTakePictureNestDone(List<PartGraphics> parts, NestAgent nest);
 	public abstract void msgTakePictureKitDone(boolean done);
 	
 	public abstract boolean pickAndExecuteAnAction();
