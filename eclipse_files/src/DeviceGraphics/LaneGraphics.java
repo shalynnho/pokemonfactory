8991a71c7efc75c86f1b25fd73949e1f610b71c3
Shay Ho <shalynkh@usc.edu>
2012-11-05 21:09:32 -0800

    updated lanegraphis

--- eclipse_files/src/DeviceGraphics/LaneGraphics.java
+++ eclipse_files/src/DeviceGraphics/LaneGraphics.java
@@ -88,7 +88,6 @@ public class LaneGraphics extends DeviceGraphics implements
 
 		partsOnLane.remove(0); // this is kind of dangerous. check that correct
 								// part is removed.
-		agentPartsOnLane.remove(0);
 		server.sendData(new Request(Constants.LANE_GIVE_PART_TO_NEST, Constants.LANE_TARGET +":"+ laneID, null));
 	}
 
@@ -155,11 +154,8 @@ public class LaneGraphics extends DeviceGraphics implements
 			laneAgent.msgReceivePartDone(agentPartsOnLane.get(agentPartsOnLane.size()-1));
 		} else if (cmd.equals(Constants.LANE_GIVE_PART_TO_NEST + Constants.DONE_SUFFIX)) {
 			laneAgent.msgGivePartToNestDone(agentPartsOnLane.get(0));
-			// TODO: laneAgent.msgReceivePartDone(part);
-			// store list of Part on lane
+			agentPartsOnLane.remove(0);
 		}
-		
-		
 	
 	}
 
