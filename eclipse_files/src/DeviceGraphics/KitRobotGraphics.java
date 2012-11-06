fbd954c69aeb51b8392253306d5de82e1f11170b
Adrian Cagaanan <cagaanan@usc.edu>
2012-11-05 19:06:36 -0800

    Testing git

--- eclipse_files/src/DeviceGraphics/KitRobotGraphics.java
+++ eclipse_files/src/DeviceGraphics/KitRobotGraphics.java
@@ -14,6 +14,7 @@ public class KitRobotGraphics extends DeviceGraphics implements GraphicsInterfac
 	private Location location;
 	private Server server;
 	
+	
 	KitGraphics[] positions;
 	
 	ArrayList<KitGraphics> kitsOnKitRobot;
