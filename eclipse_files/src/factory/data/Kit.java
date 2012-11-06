060d426ae84fb12310efd46ee496d731b5bf3781
dpaje <paje@usc.edu>
2012-11-05 21:44:51 -0800

    Fixed PartsRobot's interaction with PartsRobotGraphics if the graphical represenation wasn't set.

--- eclipse_files/src/factory/data/Kit.java
+++ eclipse_files/src/factory/data/Kit.java
@@ -9,39 +9,38 @@ public class Kit {
 	public KitGraphics kit;
 
 	public String kitID;
-	
-	public ArrayList<PartType> partsExpected= new ArrayList<PartType>();
-	
+
+	public ArrayList<PartType> partsExpected = new ArrayList<PartType>();
+
 	public ArrayList<Part> parts = new ArrayList<Part>();
 
-	public Kit(){
-		
+	public Kit() {
+
 	}
-	
+
 	public Kit(ArrayList<PartType> expected) {
-		partsExpected=expected;
+		partsExpected = expected;
 	}
 
 	public Kit(String kitID) {
 		this.kitID = kitID;
 	}
-	
-	public boolean needPart(Part part){
-		int count=0;
-		for(PartType type: partsExpected){
-			if(type == part.type){
+
+	public boolean needPart(Part part) {
+		int count = 0;
+		for (PartType type : partsExpected) {
+			if (type == part.type) {
 				count++;
 			}
 		}
-		for(Part tempPart: parts){
-			if(tempPart.type == part.type){
+		for (Part tempPart : parts) {
+			if (tempPart.type == part.type) {
 				count--;
 			}
 		}
-		if(count>0){
+		if (count > 0) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
