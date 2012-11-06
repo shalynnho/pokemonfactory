2a33f18ecfb320790aec8c208ef3f5c58f043f35
Harry Trieu <htrieu@usc.edu>
2012-11-05 22:08:51 -0800

    Removed unused functions, added request parsing, fixed semantics in NestGraphicsDisplay.

--- eclipse_files/src/DeviceGraphicsDisplay/NestGraphicsDisplay.java
+++ eclipse_files/src/DeviceGraphicsDisplay/NestGraphicsDisplay.java
@@ -1,4 +1,3 @@
-
 package DeviceGraphicsDisplay;
 
 import java.awt.Graphics2D;
@@ -22,8 +21,6 @@ import factory.data.PartType;
 public class NestGraphicsDisplay extends DeviceGraphicsDisplay {	
 	// max number of parts this Nest holds
 	private static final int MAX_PARTS=8;
-	
-	private Location nestLocation;
 	// width and height of the nest
 	private static final int NEST_WIDTH=45; 
 	private static final int NEST_HEIGHT=80;
@@ -34,12 +31,15 @@ public class NestGraphicsDisplay extends DeviceGraphicsDisplay {
 	private Client client;
 	// the id of this nest
 	private int nestID;
+	
+	private Location nestLocation;
+	
 	//boolean if the nest is full
 	private boolean isFull;
 	// dynamically stores the parts currently in the Nest
 	private ArrayList<PartGraphicsDisplay> partsInNest = new ArrayList<PartGraphicsDisplay>();
 
-	/*
+	/**
 	 * Default constructor
 	 */
 	public NestGraphicsDisplay(Client c, int id) {
@@ -61,10 +61,6 @@ public class NestGraphicsDisplay extends DeviceGraphicsDisplay {
 		}
 	}
 	
-	public void requestData(Request r) {
-		
-	}
-	
 	public void receivePart(PartGraphicsDisplay p) {
 		
 	}
@@ -80,8 +76,9 @@ public class NestGraphicsDisplay extends DeviceGraphicsDisplay {
 		
 	}
 
-
-	@Override
+	/**
+	 * Handles drawing of NestGraphicsDisplay objects
+	 */
 	public void draw(JComponent c, Graphics2D g) {		
 		g.drawImage(Constants.NEST_IMAGE, nestLocation.getX(), nestLocation.getY(), c);
 		for(PartGraphicsDisplay part : partsInNest) {
@@ -89,19 +86,31 @@ public class NestGraphicsDisplay extends DeviceGraphicsDisplay {
 		}
 	}
 
-	@Override
+	/**
+	 * Processes requests targeted at the NestGraphicsDisplay
+	 */
 	public void receiveData(Request req) {
-		// TODO Auto-generated method stub
+		if (req.getCommand().equals(Constants.NEST_RECEIVE_PART_COMMAND)) {
+			// TODO code to handle command
+			// send a request back to the server saying done
+			client.sendData(new Request(Constants.NEST_RECEIVE_PART_COMMAND + Constants.DONE_SUFFIX, Constants.NEST_TARGET, null));
+		} else if (req.getCommand().equals(Constants.NEST_GIVE_TO_PART_ROBOT_COMMAND + Constants.DONE_SUFFIX)) {
+			// TODO code to handle command
+			// send a request back to the server saying done
+			client.sendData(new Request(Constants.NEST_GIVE_TO_PART_ROBOT_COMMAND + Constants.DONE_SUFFIX, Constants.NEST_TARGET, null));
+		} else if (req.getCommand().equals(Constants.NEST_PURGING_DONE_COMMAND + Constants.DONE_SUFFIX)) {
+			// TODO code to handle command
+			// send a request back to the server saying done
+			client.sendData(new Request(Constants.NEST_PURGING_DONE_COMMAND + Constants.DONE_SUFFIX, Constants.NEST_TARGET, null));
+		}
 		
 	}
 
-
-	@Override
+	/**
+	 * Allows another object to set the NestGraphicsDisplay location
+	 */
 	public void setLocation(Location newLocation) {
 		// TODO Auto-generated method stub
 		
 	}
-	
-
 }
-
