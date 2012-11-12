9702c4d86678c9f7c2c998d3bc7e778ed6ed7568
Chris Gebert <cgebert@usc.edu>
2012-11-11 19:24:24 -0800

    Updated GantryGraphics' GraphicsInterface

--- eclipse_files/src/GraphicsInterfaces/GantryGraphics.java
+++ eclipse_files/src/GraphicsInterfaces/GantryGraphics.java
@@ -1,11 +1,13 @@
 package GraphicsInterfaces;
 
-import DeviceGraphics.BinGraphics;
+import agent.FeederAgent;
+import agent.data.Bin;
 
 public interface GantryGraphics {
 
-	public abstract void receiveBin(BinGraphics bin);
-	public abstract void dropBin(BinGraphics bin, FeederGraphics feeder);
-	public abstract void removeBin(BinGraphics bin);
+	public abstract void receiveBin(Bin newBin, FeederAgent feeder);
+	public abstract void dropBin(Bin newBin, FeederAgent feeder);
+	public abstract void removeBin(Bin newBin);
+	public abstract void hereIsNewBin(Bin bin);
 	
 }
