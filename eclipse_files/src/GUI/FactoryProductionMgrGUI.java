b2710440888d3971f0145a82a029424f2ddb1d0c
shalynnho <shalynkh@usc.edu>
2012-11-11 18:44:34 -0800

    FPM GUI

--- eclipse_files/src/GUI/FactoryProductionMgrGUI.java
+++ eclipse_files/src/GUI/FactoryProductionMgrGUI.java
@@ -1,8 +1,7 @@
 package GUI;
 
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.*;
 
@@ -15,20 +14,55 @@ public class FactoryProductionMgrGUI extends OverlayPanel implements ActionListe
 		setPreferredSize(new Dimension(PANEL_WIDTH, height));
 		setMinimumSize(new Dimension(PANEL_WIDTH, height));
 		setMaximumSize(new Dimension(PANEL_WIDTH, height));
-		
 		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		
+		setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
 		// TODO: get array of possible kits from Kit Assembly Manager
-		JComboBox kitComboBox = new JComboBox();
+		JComboBox<String> kitComboBox = new JComboBox<String>();
+		// TODO: change this, 
+		kitComboBox.addItem("DEFAULT KIT");
+		c.gridx = 0;
+		c.gridy = 0;
+		//c.ipady = 20;
+		c.anchor = GridBagConstraints.PAGE_START;
+		add(kitComboBox, c);
 		
-		kitComboBox.addItem(arg0)
+		SpinnerNumberModel spinModel = new SpinnerNumberModel(0, 0, 1000, 1);
+		JSpinner numSpinner = new JSpinner(spinModel);
+		c.gridx = 0;
+		c.gridy = 1;
+		c.anchor = GridBagConstraints.CENTER;
+		add(numSpinner, c);
 		
+		JButton orderButton = new JButton("ORDER KITS");
+		orderButton.addActionListener(this);
+		c.gridx = 0;
+		c.gridy = 2;
+		c.anchor = GridBagConstraints.PAGE_END;
+		add(orderButton, c);
+				
+		JTextArea kitSchedule = new JTextArea();
+		kitSchedule.setColumns(20);
+		kitSchedule.setRows(20);
+		kitSchedule.setLineWrap(true);
+		kitSchedule.setEditable(false);
+		kitSchedule.setWrapStyleWord(true);
+		JScrollPane scrollPane = new JScrollPane(kitSchedule);
+		c.gridx = 0;
+		c.gridy = 3;
+		c.gridwidth = GridBagConstraints.REMAINDER;
+		c.gridheight = GridBagConstraints.REMAINDER;
+		c.anchor = GridBagConstraints.PAGE_END;
+		add(scrollPane, c);
+				
 	}
 
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+		// TODO: send message to FCS with order info
 		
 	}
 }
