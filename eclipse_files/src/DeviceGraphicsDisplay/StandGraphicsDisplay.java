c5b62bc2dc544eb8c254d0b523c68cc4762027d4
shalynnho <shalynkh@usc.edu>
2012-11-11 20:45:21 -0800

    Added StandGraphicsDisplay

---
+++ eclipse_files/src/DeviceGraphicsDisplay/StandGraphicsDisplay.java
@@ -0,0 +1,60 @@
+package DeviceGraphicsDisplay;
+
+import java.awt.Graphics2D;
+
+import javax.swing.JComponent;
+
+import Networking.Request;
+import Utils.Location;
+
+public class StandGraphicsDisplay extends DeviceGraphicsDisplay {
+	
+	// the kit that is currently on the stand
+	private KitGraphicsDisplay kit;
+	private boolean isEmpty;
+	
+	
+	private StandGraphicsDisplay() {
+		isEmpty = true;
+	}
+	
+	public void giveKit(KitGraphicsDisplay kgd) {
+		
+	}
+	
+	public void receiveKit() {
+		
+	}
+	
+	public void receivePart() {
+		
+	}
+
+
+	@Override
+	public void draw(JComponent c, Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receiveData(Request req) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setLocation(Location newLocation) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
