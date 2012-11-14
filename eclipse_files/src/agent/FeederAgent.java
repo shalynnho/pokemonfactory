f3b62ac7363c372a3b97d452158b21e3b8ccc0fb
Arjun Bhargava <arjunbha@usc.edu>
2012-11-14 14:42:43 -0800

    added a break

--- eclipse_files/src/agent/FeederAgent.java
+++ eclipse_files/src/agent/FeederAgent.java
@@ -105,6 +105,7 @@ public class FeederAgent extends Agent implements Feeder {
 				print("lane type is " + lane.type.toString());
 				lane.state = LaneStatus.GIVING_PARTS;
 				state = FeederStatus.FEEDING_PARTS;
+				break;
 			}
 		}
 		}
