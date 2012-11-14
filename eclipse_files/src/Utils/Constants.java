3358d24d32b5aaaf4080a0c42f0d0a1f3c7e9d2e
Matthew Zecchini <zecchini@usc.edu>
2012-11-13 18:18:52 -0800

    Test Commit to see if account shows up
    just added a space… didn't make a change

--- eclipse_files/src/Utils/Constants.java
+++ eclipse_files/src/Utils/Constants.java
@@ -60,7 +60,7 @@ public abstract class Constants {
 	public static final Location CONVEYOR_LOC = new Location(0,185);
 	public static final Location KIT_LOC = new Location(20,200);
 	public static final Location PARTS_ROBOT_LOC = new Location(250,450);
-	public static final Location GANTRY_ROBOT_LOC = new Location(500,100);
+	public static final Location GANTRY_ROBOT_LOC = new Location(500,100); 
 	
 	// TODO: get exact location coordinates
 	public static final Location BIN_STORAGE_LOC = new Location(800,1000);
