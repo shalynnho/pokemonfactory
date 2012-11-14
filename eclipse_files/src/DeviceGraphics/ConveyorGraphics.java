b8572c354d811f48c3c2b40d709390bdcb7d35ec
Neetu George <neetugeo@usc.cedu>
2012-11-13 19:05:34 -0800

    Fixed more animation issues

--- eclipse_files/src/DeviceGraphics/ConveyorGraphics.java
+++ eclipse_files/src/DeviceGraphics/ConveyorGraphics.java
@@ -44,15 +44,8 @@ public class ConveyorGraphics implements GraphicsInterfaces.ConveyorGraphics,
 	}
 
 	public void giveKitToKitRobot() {
-
-		// sending the kit to be taken away to KitRobotGraphics
-		// server.sendData(new Request(
-		// Constants.CONVEYOR_GIVE_KIT_TO_KIT_ROBOT_COMMAND,
-		// Constants.CONVEYOR_TARGET, null)); // temporary command name
-		// until Kit Robot finalized
-		// server.sendData(new Request("GetThisKit", ))
+		server.sendData(new Request(Constants.CONVEYOR_GIVE_KIT_TO_KIT_ROBOT_COMMAND,Constants.CONVEYOR_TARGET, null)); // temporary command name until Kit Robot finalized
 		kitsOnConveyor.remove(0);
-
 	}
 
 	/**
@@ -94,7 +87,7 @@ public class ConveyorGraphics implements GraphicsInterfaces.ConveyorGraphics,
 				// Request(Constants.CONVEYOR_MAKE_NEW_KIT_COMMAND,
 				// Constants.CONVEYOR_TARGET, null));
 				StandAgent stand = (StandAgent) server.agents.get("Stand");
-				stand.msgMakeKits(5);
+				stand.msgMakeKits(1);
 			} else if (command.equals(Constants.CONVEYOR_MAKE_NEW_KIT_COMMAND
 					+ Constants.DONE_SUFFIX)) {
 				conveyorAgent.msgBringEmptyKitDone();
