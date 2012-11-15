4c17b4ac219ca79d637a3030e5eccba4c1105526
Ross Newman <rcnewman@usc.edu>
2012-11-14 20:13:03 -0800

    MockPartsRobotGraphics

---
+++ eclipse_files/src/agent/test/mock/MockPartsRobotGraphics.java
@@ -0,0 +1,40 @@
+package agent.test.mock;
+
+import DeviceGraphics.DeviceGraphics;
+import DeviceGraphics.KitGraphics;
+import DeviceGraphics.PartGraphics;
+import GraphicsInterfaces.PartsRobotGraphics;
+import Networking.Request;
+
+public class MockPartsRobotGraphics extends MockAgent implements PartsRobotGraphics, DeviceGraphics{
+
+	public EventLog log;
+	public MockPartsRobotGraphics(String name, EventLog log) {
+		super(name, log);
+		this.log = super.getLog();
+	}
+
+	@Override
+	public void receiveData(Request req) {
+
+		log.add(new LoggedEvent("Received message msgreceiveData"));
+		
+	}
+
+	@Override
+	public void pickUpPart(PartGraphics part) {
+
+		log.add(new LoggedEvent("Received message msgpickUpPart"));
+		
+	}
+
+	@Override
+	public void givePartToKit(PartGraphics part, KitGraphics kit) {
+
+		log.add(new LoggedEvent("Received message msgPartToKit"));
+		
+	}
+
+	
+
+}
