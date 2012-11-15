d4bb8b0cb21102fdb82efcb0dd366b02b622b7d0
shalynnho <shalynkh@usc.edu>
2012-11-14 19:23:39 -0800

    method cleanup

--- eclipse_files/src/DeviceGraphicsDisplay/LaneGraphicsDisplay.java
+++ eclipse_files/src/DeviceGraphicsDisplay/LaneGraphicsDisplay.java
@@ -299,12 +299,4 @@ public class LaneGraphicsDisplay extends DeviceGraphicsDisplay {
 		}
 	}
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
