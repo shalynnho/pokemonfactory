7e347b31d7c3ba228af9cd088bee7ce91e7bd7fa
Peter Zhang <p@peetahzee.com>
2012-11-16 18:00:45 -0800

    fix to send message to gantry

--- eclipse_files/src/Networking/Server.java
+++ eclipse_files/src/Networking/Server.java
@@ -354,6 +354,8 @@ public class Server {
 			sendDataToPartsRobot(req);
 		} else if (target.contains(Constants.ALL_TARGET)) {
 			sendDataToGUIManagers(req);
+		} else if (target.contains(Constants.GANTRY_ROBOT_TARGET)) {
+			sendDataToGantry(req);
 		}
 	}
 
@@ -404,6 +406,10 @@ public class Server {
 	private void sendDataToLane(Request req) {
 		factProdMngrWriter.sendData(req);
 		laneMngrWriter.sendData(req);
+	}
+	
+	private void sendDataToGantry(Request req) {
+		factProdMngrWriter.sendData(req);
 	}
 
 	public static void main(String[] args) {
