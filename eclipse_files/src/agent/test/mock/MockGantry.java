94f1e351bc9ed6335bf578c320debb617b387685
dpaje <paje@usc.edu>
2012-11-16 18:20:03 -0800

    Added unimplemented messages to MockGantry

--- eclipse_files/src/agent/test/mock/MockGantry.java
+++ eclipse_files/src/agent/test/mock/MockGantry.java
@@ -2,8 +2,8 @@ package agent.test.mock;
 
 import agent.FeederAgent;
 import agent.data.Bin;
-import factory.PartType;
 import agent.interfaces.Gantry;
+import factory.PartType;
 
 /**
  * Mock Gantry
@@ -69,6 +69,12 @@ public class MockGantry extends MockAgent implements Gantry {
 
 	@Override
 	public void discardBin(Bin bin) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void msgRemoveBin(Bin bin) {
 		// TODO Auto-generated method stub
 
 	}
