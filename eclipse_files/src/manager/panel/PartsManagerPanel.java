1e5108f337be786def2bc2c0bc711858979b9ba4
Aaron Harris <contactaaronh@gmail.com>
2012-11-18 15:36:33 -0800

    PartManagerPanel: removed unnecessary imports and removed FileChooser

--- eclipse_files/src/manager/panel/PartsManagerPanel.java
+++ eclipse_files/src/manager/panel/PartsManagerPanel.java
@@ -10,8 +10,6 @@ import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -42,7 +40,6 @@ public class PartsManagerPanel extends JPanel implements ActionListener {
 	private JPanel pnlAdd;
 	private JComboBox cbPart;
 	private String[] backupFields; // used for temporarily storing old field data in case a user wants to revert
-	private final JFileChooser fc;
 	private JTextField tfName;
 	
 	/**
@@ -50,7 +47,6 @@ public class PartsManagerPanel extends JPanel implements ActionListener {
 	 */
 	public PartsManagerPanel() {
 		setLayout(new GridLayout(1, 1));
-		fc = new JFileChooser();
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		JPanel managerPanel = new JPanel();
