2a696a3302f3e84313283831c7cc190c551aaf13
Chris Gebert <cgebert@usc.edu>
2012-11-17 17:37:34 -0800

    Updated Gantry location and cleaned it up a little. Changed inital bin locations

--- eclipse_files/src/Utils/Constants.java
+++ eclipse_files/src/Utils/Constants.java
@@ -70,7 +70,7 @@ public abstract class Constants {
 	public static final Location CONVEYOR_LOC = new Location(0, 80); // 185
 	public static final Location KIT_LOC = new Location(150, 200);
 	public static final Location PARTS_ROBOT_LOC = new Location(250, 450);
-	public static final Location GANTRY_ROBOT_LOC = new Location(885, 180);
+	public static final Location GANTRY_ROBOT_LOC = new Location(940, 180);
 
 	// TODO: get exact location coordinates
 	public static final Location BIN_STORAGE_LOC = new Location(800, 1000);
