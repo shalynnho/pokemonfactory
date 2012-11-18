9752d335e15572f0fd8288c96e6735b729114654
Peter Zhang <p@peetahzee.com>
2012-11-17 20:53:48 -0800

    scrollable panels in FPM. Getting rid of old non-panel codes

--- eclipse_files/src/manager/panel/FactoryProductionManagerPanel.java
+++ eclipse_files/src/manager/panel/FactoryProductionManagerPanel.java
@@ -4,19 +4,15 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
-import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
-import javax.swing.JTextArea;
 import javax.swing.SpinnerNumberModel;
 
 import manager.FactoryProductionManager;
@@ -40,8 +36,6 @@ public class FactoryProductionManagerPanel extends OverlayInternalFrame implemen
 	// A reference to the FactoryProductionManager client
 	private FactoryProductionManager fpmClient;
 	
-	// Stores kits currently available for order
-	private ArrayList<KitConfig> kitConfigs = new ArrayList<KitConfig>();
 	// Stores current list of orders
 	private ArrayList<Order> queue = new ArrayList<Order>();
 	// Stores the selected kitConfig for a new order
@@ -50,17 +44,14 @@ public class FactoryProductionManagerPanel extends OverlayInternalFrame implemen
 	private Order selectedOrder;
 	
 	/** JComponents **/
-	// Displays kits currently available for order
-	private JComboBox kitComboBox;
-	private DefaultComboBoxModel defaultComboBox;
 	// Displays current schedule of orders
-	private JTextArea orderScheduleTextArea;
 	private SpinnerNumberModel spinnerModel;
 	private JSpinner quantitySpinner;
 	private JButton orderButton;
-	private JScrollPane orderScrollPane;
-	private OrdersListPanel ordersPanel;
 	private KitsListPanel kitsPanel;
+	private JScrollPane kitsScrollPane;
+	private OrdersListPanel ordersPanel;
+	private JScrollPane ordersScrollPane;	
 	
 	private int height;
 	
@@ -82,25 +73,6 @@ public class FactoryProductionManagerPanel extends OverlayInternalFrame implemen
 		setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 	
-		/*
-		kitComboBox = new JComboBox();
-		
-		for (int i = 0; i < kitConfigs.size(); i++) {
-			kitComboBox.addItem(kitConfigs.get(i).getName());
-		}
-		
-		c.gridx = 0;
-		c.gridy = 0;
-		c.anchor = GridBagConstraints.PAGE_START;
-		add(kitComboBox, c);
-		kitComboBox.addMouseListener(this);
-		for (int i = 0; i < kitComboBox.getComponentCount(); i++) {
-			Component comp = kitComboBox.getComponent(i);
-			comp.addMouseListener(this);
-		}
-		((JTextField)kitComboBox.getEditor().getEditorComponent()).addMouseListener(this);
-		*/
-		
 		// Setup KitsListPanel
 		kitsPanel = new KitsListPanel(new KitSelectHandler() {
 			@Override
@@ -113,23 +85,28 @@ public class FactoryProductionManagerPanel extends OverlayInternalFrame implemen
 		kitsPanel.setBackground(new Color(0, 0, 0, 30));
 		addMouseListeners(kitsPanel);
 		
+		kitsScrollPane = new JScrollPane(kitsPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		kitsScrollPane.setOpaque(false);
+		kitsScrollPane.getViewport().setOpaque(false);
+		
 		c.gridx = 0;
 		c.gridy = 0;
 		c.gridwidth = GridBagConstraints.REMAINDER;
 		c.gridheight = 2;
-		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weighty = 1;
+		c.fill = GridBagConstraints.BOTH;
 		
-		add(kitsPanel, c);
+		add(kitsScrollPane, c);
 		
 		// Setup JSpinner
 		spinnerModel = new SpinnerNumberModel(0, 0, 1000, 1);
 	    quantitySpinner = new JSpinner(spinnerModel);
 		c.gridx = 0;
 		c.gridy = 2;
-		// Top padding
-		c.insets = new Insets(50,0,0,0);
 		c.gridheight = 1;
+		c.weighty = 0;
 		c.anchor = GridBagConstraints.CENTER;
+		c.fill = GridBagConstraints.HORIZONTAL;
 		add(quantitySpinner, c);
 		// Add listener to nested components - for disappearing panel
 		for (int i = 0; i < quantitySpinner.getComponentCount(); i++) {
@@ -143,33 +120,6 @@ public class FactoryProductionManagerPanel extends OverlayInternalFrame implemen
 		c.gridx = 0;
 		c.gridy = 3;
 		add(orderButton, c);
-//		orderButton.addMouseListener(this);
-		
-		/*
-		orderScheduleTextArea = new JTextArea();
-		orderScheduleTextArea.setColumns(20);
-		orderScheduleTextArea.setRows(20);
-		orderScheduleTextArea.setLineWrap(true);
-		orderScheduleTextArea.setEditable(false);
-		orderScheduleTextArea.setWrapStyleWord(true);
-		orderScheduleTextArea.addMouseListener(this);
-		
-		orderScrollPane = new JScrollPane(orderScheduleTextArea);
-		c.gridx = 0;
-		c.gridy = 4;
-		c.gridwidth = GridBagConstraints.REMAINDER;
-		c.gridheight = GridBagConstraints.REMAINDER;
-		c.anchor = GridBagConstraints.PAGE_END;
-
-		c.weightx = 1;
-		c.weighty = 1;
-		c.fill = GridBagConstraints.BOTH;
-		add(orderScrollPane, c);
-		orderScrollPane.addMouseListener(this);
-		for (int i = 0; i < orderScrollPane.getComponentCount(); i++) {
-			orderScrollPane.getComponents()[i].addMouseListener(this);
-		}
-		*/
 		
 		// Setup OrdersListPanel
 		OrdersListPanel.OrderSelectHandler selectHandler = new OrdersListPanel.OrderSelectHandler() {
@@ -182,6 +132,10 @@ public class FactoryProductionManagerPanel extends OverlayInternalFrame implemen
 		ordersPanel.setVisible(true);
 		ordersPanel.setBackground(new Color(0, 0, 0, 30));
 		
+		ordersScrollPane = new JScrollPane(ordersPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		ordersScrollPane.setOpaque(false);
+		ordersScrollPane.getViewport().setOpaque(false);
+		
 		c.gridx = 0;
 		c.gridy = 4;
 		c.gridwidth = GridBagConstraints.REMAINDER;
@@ -191,7 +145,7 @@ public class FactoryProductionManagerPanel extends OverlayInternalFrame implemen
 		c.weightx = 1;
 		c.weighty = 1;
 		c.fill = GridBagConstraints.BOTH;
-		add(ordersPanel, c);
+		add(ordersScrollPane, c);
 		
 		// Add mouseListener to second-level components
 		for (int i = 0; i < getComponentCount(); i++) {
@@ -231,19 +185,8 @@ public class FactoryProductionManagerPanel extends OverlayInternalFrame implemen
 	 * @param kc ArrayList of current KitConfigs
 	 */
 	public void updateKitConfigs(ArrayList<KitConfig> kc) {
-		kitConfigs = kc;
 		kitsPanel.updateList(kc);
 		addMouseListeners(kitsPanel);
-		
-		/*
-		// Clear the JComboBox
-		defaultComboBox.removeAllElements();
-		
-		// Populate the JComboBox with the new kitConfigs
-		for (int i = 0; i < kitConfigs.size(); i++) {
-			kitComboBox.addItem(kitConfigs.get(i).getName());
-		}
-		*/
 	}
 	
 	/**
@@ -255,22 +198,6 @@ public class FactoryProductionManagerPanel extends OverlayInternalFrame implemen
 		System.out.println("Queue size: " + queue.size());
 		ordersPanel.updateList(o);
 		addMouseListeners(ordersPanel);
-
-		/*
-		// Clear the contents of the JTextArea
-		orderScheduleTextArea.setText("");
-		
-		System.out.println("Queue size: " + queue.size());
-		
-		// Populate the JTextArea with current orders
-		for (int i = 0; i < queue.size(); i++) {
-			// System.out.println("--------- Orders in the Queue ----------");
-			// System.out.println("Kit name: " + queue.get(i).getConfig().getName() + " // Quantity: " + queue.get(i).getNumKits());
-			
-			orderScheduleTextArea.append("Kit Type: " + queue.get(i).getConfig().getName() + "  |  Quantity: " + queue.get(i).getNumKits() + "\n");
-		}
-		*/
-		
 	}
 	
 	/**
