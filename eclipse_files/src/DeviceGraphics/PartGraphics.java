ced04829692f38bc85fe80da285492262a18ab60
Vansh Jain <vanshjain@gmail.com>
2012-11-27 19:00:57 -0800

    changed PartsGraphics isBad boolean to quality

--- eclipse_files/src/DeviceGraphics/PartGraphics.java
+++ eclipse_files/src/DeviceGraphics/PartGraphics.java
@@ -8,34 +8,34 @@ import factory.PartType;
 public class PartGraphics implements Serializable {
     PartType partType;
     Location partLocation;
-    boolean isBad = false;
+    boolean quality = true;
 
     public PartGraphics(PartType type) {
-	partType = type;
+    	partType = type;
     }
 
     public PartGraphics(PartGraphics partGraphics) {
-	this.partLocation = partGraphics.partLocation;
-	this.partType = partGraphics.partType;
+    	this.partLocation = partGraphics.partLocation;
+    	this.partType = partGraphics.partType;
     }
 
     public void setLocation(Location newLocation) {
-	partLocation = newLocation;
+    	partLocation = newLocation;
     }
 
     public Location getLocation() {
-	return partLocation;
+    	return partLocation;
     }
 
     public PartType getPartType() {
-	return partType;
+    	return partType;
     }
 
-    public void setBad(boolean bad) {
-	isBad = bad;
+    public void setQuality(boolean qual) {
+    	quality=qual;
     }
 
     public boolean getQuality() {
-	return isBad;
+    	return quality;
     }
 }
