50d4a069955049c59905fcf491c17cfb06105b31
James Li <jameswli@usc.edu>
2012-12-01 16:52:45 -0800

    added constants for dropping part

--- eclipse_files/src/GraphicsInterfaces/PartsRobotGraphics.java
+++ eclipse_files/src/GraphicsInterfaces/PartsRobotGraphics.java
@@ -9,6 +9,6 @@ public interface PartsRobotGraphics {
 
 	public abstract void givePartToKit(PartGraphics part, KitGraphics kit, int arm);
 
-	public abstract void dropPartFromArm(PartGraphics part);
+	public abstract void dropPartFromArm(PartGraphics part, int arm);
 
 }
